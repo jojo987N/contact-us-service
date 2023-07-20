2fa46ac89b16a7949d9cd04bca13566a0c7fdf42
jemba narcisse <cabliveer@gmail.com>
2023-07-20 10:20:26 +0200

    save

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+
+var server = express()
+
+// console.log(123344)
+
+// i = 0
+// setInterval(() => {
+//     console.log(i)
+//     i+=1
+// }, 1000)
+
+server.get("/", (req, res) => {
+
+    res.send('GII')
+    // alert('Hi')
+
+})
+
+server.get("/fetch", async (req, res) => {
+
+    // res.send('GII1');
+    // alert('Hi')
+    // res.setHeader('Content-Type', 'text/xml');
+    response = await fetch('https://actucameroun.com/rss');
+    // response = await fetch('../feed1.txt');
+    textResponse = await response.text();
+    // console.log(textResponse)
+    res.send(textResponse)
+    
+    
+
+})
+
+server.listen(3000)
